35eb28929b3d10eb077aa86f56995f9d771a7787
Moon Antonio <pinchodev91@gmail.com>
2019-09-02 02:08:43 +0200

    Reubicacion general

---
+++ hola.h
@@ -0,0 +1,6 @@
+#ifndef HOLA_H_INCLUDED
+#define HOLA_H_INCLUDED
+
+void hola();
+
+#endif // HOLA_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hola.h"
+
+using namespace std;
+
+int main()
+{
+    hola();
+    return 0;
+}
